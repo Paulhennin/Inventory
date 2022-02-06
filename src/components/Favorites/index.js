3060501c52850458048c3b457848ee586bc332f0
Paulhennin <henninpaul@gmail.com>
2022-02-06 18:09:07 +0100

    fix crash compilation

--- src/components/Favorites/index.js
+++ src/components/Favorites/index.js
@@ -1,8 +1,6 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-import './style.scss';
-
 export default function Favorites() {
   return (
     <div>Page for Favorites templates</div>
--- src/components/Inventories/index.js
+++ src/components/Inventories/index.js
@@ -4,7 +4,7 @@ import PropTypes from 'prop-types';
 export default function Inventories() {
   return (
     <>
-      <div className="title">Home Content</div>
+      <div className="title">Page de tous les inventaires</div>
       <h1>Composant : App</h1>
     </>
   );
