a0d90900d98f722f4910817bd771aeda8551c191
henninpaul <paul.hennin@fasst.io>
2022-02-18 22:18:15 +0100

    :construction: :construction: Front form for newInventory

--- src/components/form/Input/index.js
+++ src/components/form/Input/index.js
@@ -6,8 +6,11 @@ export default function Input({
   name,
   type,
   classname,
+  inputClassName,
+  errorClassName,
   value,
   onChange,
+
   error,
 }) {
   return (
@@ -20,17 +23,17 @@ export default function Input({
           type={type}
           id={name}
           name={name}
-          className="lineinput-input"
+          className={inputClassName}
           placeholder={placeholder}
           value={value}
           onChange={onChange}
-          />
+        />
         <span className="lineinput-line-1" />
         <span className="lineinput-line-2" />
         <span className="lineinput-line-3" />
         <span className="lineinput-line-4" />
       </div>
-      {error && <div>{error}</div>}
+      {error && <div className={errorClassName}>{error}</div>}
     </>
   );
 }
--- src/pages/NewInventory/index.js
+++ src/pages/NewInventory/index.js
@@ -1,16 +1,28 @@
 import React, { useState } from 'react';
 import PropTypes from 'prop-types';
-import { assoc, times, assocPath } from 'ramda';
-import validate from './lib/validate'
+import {
+  assoc,
+  times,
+  assocPath,
+  pipe,
+} from 'ramda';
 import Input from 'src/components/form/Input';
 
 export default function NewInventory() {
   const [state, setState] = useState({
     inventoryName: '',
-    nbBags: null,
+    nbBags: '',
     bags: {},
     error: null,
   });
+  const handleNumberOnChange = (v) => {
+    setState(assoc('error', null));
+    const inputParsed = parseInt(v.target.value, 10);
+    console.log(typeof inputParsed);
+    if (Number.isNaN(inputParsed)) return setState(assoc('error', 'Doit être un nombre'));
+    if (inputParsed > 10) return setState(pipe(assoc('error', 'Vous ne pouvez pas avoir plus de 10 bagages'), assoc('nbBags', 10)));
+    else setState(assoc('nbBags', inputParsed)); 
+  };
 
   return (
     <div className="new-inventory-section">
@@ -25,20 +37,23 @@ export default function NewInventory() {
           type="text"
           classname="lineinput"
           placeholder="Entrez le nom de votre liste"
+          inputClassName="lineinput-input"
           value={state.inventoryName}
           onChange={(v) => setState(assoc('inventoryName', v.target.value))}
         />
         <Input
           name="nbBags"
           labelValue="Nombre de sacs"
-          type="number"
+          type="text"
           classname="lineinput"
           placeholder="Entrez le nombre de sacs"
           value={state.nbBags}
-          onChange={(v) => validate(v, 'nbBags', setState)}
+          onChange={handleNumberOnChange}
+          inputClassName="lineinput-input input-number"
           error={state.error}
+          errorClassName="new-inventory-section_error"
         />
-        {state.nbBags && times(index => {
+        {state.nbBags && times((index) => {
           console.log('');
           return (
             <Input
@@ -46,6 +61,7 @@ export default function NewInventory() {
               name="nbBags"
               type="text"
               classname="lineinput"
+              inputClassName="lineinput-input"
               placeholder="Type de sac"
               value={state.bags[index + 1]}
               onChange={(v) => setState(assocPath(['bags', `${index + 1}`], v.target.value))}
